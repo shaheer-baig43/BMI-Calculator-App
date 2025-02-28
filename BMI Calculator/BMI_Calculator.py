8eaeed2228d24dee6cd6ee6a5d422cd428384cfe
shaheer-baig43 <shaheerbaig43@gmail.com>
2025-03-01 01:32:41 +0500

    Add files via upload

---
+++ BMI Calculator/BMI_Calculator.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pandas as pd
+
+st.title('BMI calculator created by Shaheer')
+
+
+height = st.slider('Enter your Height (in cm): ', 100, 250, 175)
+weight = st.slider('Enter your Weight (in kg): ', 30, 200, 70)
+
+bmi = weight/(height/100) ** 2
+
+
+st.write(f'Your BMI is: {bmi:.2f}')
+
+st.write('### BMI Categories ###')
+
+st.write('- Underweight: BMI less than 18.5')
+st.write('- Normal weight: BMI between 18.5 and 24.9')
+st.write('- Overweight: BMI between 25 and 29.9')
+st.write('- Obesity: BMI of 30 or more')
+
